1cc21d2e9fd2eb8a573d469844845ac85f04667f
Nawar <dragonblue327@gmail.com>
2024-07-31 16:07:13 +0300

    change gitignore

--- Note.Application/obj/Debug/net7.0/Note.Application.AssemblyInfo.cs
+++ Note.Application/obj/Debug/net7.0/Note.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Note.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172f05632d8a55d076f04786a31324d51c330620")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56d1a14e3722555efe8e14f6f3859b7d70d6f969")]
 [assembly: System.Reflection.AssemblyProductAttribute("Note.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Note.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
